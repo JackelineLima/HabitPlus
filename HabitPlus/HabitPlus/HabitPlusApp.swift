188f015a6f0c3726393349dba7ce7ba703ac8dd3
Jackeline Pires de Lima <jackeline.lima@e-unicred.com.br>
2024-08-30 18:14:19 -0300

    Adiciona projeto

---
+++ HabitPlus/HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Jacqueline Lima on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+          SplashView()
+        }
+    }
+}
---
+++ HabitPlus/HabitPlus/Splash/SplashUIState.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum SplashUIState {
+  case loading
+  case goToSignInScreen
+  case goToHome
+  case error(String)
+}
