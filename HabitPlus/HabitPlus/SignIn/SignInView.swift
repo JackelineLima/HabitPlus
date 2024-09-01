515ff1e10aa7b6d9bd0661f99003bf51a67d6e34
Jackeline Pires de Lima <jackeline.lima@e-unicred.com.br>
2024-09-01 19:31:12 -0300

    Adiciona layout inicial da tela de signin

--- HabitPlus/HabitPlus/SignIn/SignInView.swift
+++ HabitPlus/HabitPlus/SignIn/SignInView.swift
@@ -4,8 +4,107 @@ struct SignInView: View {
   
   @ObservedObject var viewModel: SignInViewModel
   
+  @State var email = ""
+  @State var password = ""
+  @State var action: Int? = 0
+  @State var navigationHidden = true
+  
   var body: some View {
-    Text("Tela de signIn")
+    NavigationView {
+      
+      ScrollView(showsIndicators: false) {
+        
+        VStack(alignment: .center, spacing: 20) {
+        
+          Spacer(minLength: 36)
+      
+          VStack(alignment: .center, spacing: 8) {
+            Image("logo")
+              .resizable()
+              .scaledToFit()
+              .padding(.horizontal, 48)
+            
+            Text("Login")
+              .foregroundColor(.orange)
+              .font(Font.system(.title).bold())
+              .padding(.bottom, 8)
+            
+            numberField
+            
+            passwordField
+            
+            enterButton
+            
+            registerLink
+            
+            Text("Copyright @YYY")
+              .foregroundColor(Color.gray)
+              .font(Font.system(size: 16).bold())
+              .padding(.top, 16)
+          }
+          
+        }
+      
+      }
+      .frame(maxWidth:.infinity, maxHeight: .infinity)
+      .padding(.horizontal, 32)
+      .background(Color.white)
+      .navigationBarTitle("Login", displayMode: .inline)
+      .navigationBarHidden(navigationHidden)
+    }
+    .onAppear {
+      self.navigationHidden = true
+    }
+    .onDisappear {
+      self.navigationHidden = false
+    }
+    
+  }
+  
+}
+
+extension SignInView {
+  var numberField: some View {
+    TextField("", text: $email)
+      .border(Color.black)
+  }
+}
+
+extension SignInView {
+  var passwordField: some View {
+    SecureField("", text: $password)
+      .border(Color.orange)
+  }
+}
+
+extension SignInView {
+  var enterButton: some View {
+    Button("Entrar") {
+      // acontece o evento de click
+    }
+  }
+}
+
+extension SignInView {
+  var registerLink: some View {
+    VStack {
+      Text("Ainda não possui um login ativo?")
+        .foregroundColor(.gray)
+        .padding(.top, 48)
+      
+      ZStack {
+        NavigationLink(
+          destination: Text("Tela de cadastro"),
+          tag: 1,
+          selection: $action,
+          label: { EmptyView() })
+        
+        Button("Realize seu Cadastro") {
+          self.action = 1
+        }
+      }
+      
+    }
   }
 }
 
